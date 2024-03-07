1ccabfa352c7966709aa5d0edbbcae97155423f5
Khussain <khussain.qudaibergenov@gmail.com>
2024-03-08 03:13:10 +0500

    update

--- app_sender/internal/entity/api/goods.go
+++ app_sender/internal/entity/api/goods.go
@@ -2,5 +2,5 @@ package api
 
 type GoodsReq struct {
 	Name        string `json:"name" db:"name" binding:"required"`
-	Description string `json:"description" db:"description" binding:"required"`
+	Description string `json:"description" db:"description"`
 }
